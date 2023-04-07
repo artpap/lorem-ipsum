a62760820102d04c21d4f6839504aa68710bdcba
artpap <100701975+artpap@users.noreply.github.com>
2023-04-07 15:17:47 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const slicedText = data.slice(0, count);
+    setText(slicedText);
+  };
+
+  return (
+    <section className='section-center'>
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min='1'
+          step='1'
+          max='8'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          generate
+        </button>
+      </form>
+
+      <article className='lorem-text'>
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
